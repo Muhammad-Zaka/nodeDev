3ed4dd6aa4939183c34118e0347c3aa8b5ff3eea
unknown <m.zkaurrehman@gmail.com>
2020-09-05 03:15:05 +0500

    Added Middleware

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
+//const logger = require('./middleware/logger')
+const morgan = require('morgan')
+
 //Route files
 const bootcamps = require('./routes/bootcamps');
 
@@ -8,6 +11,12 @@ dotenv.config({path: './config/config.env'});
 
 const app = express();
 
+
+//dev logging middleware
+if(process.env.NODE_ENV== 'development'){
+
+    app.use(morgan('dev'));
+}
 
 //Mount Routers
 app.use('/api/v1/bootcamps', bootcamps);
